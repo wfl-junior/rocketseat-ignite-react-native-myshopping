c8524e58a877907ae47e1de1df452f8ab4c22983
Wallace Júnior <wflj1997@hotmail.com>
2022-07-21 23:36:23 -0300

    Atualizando um documento

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,42 +1,47 @@
-import React from 'react';
-
-import { ButtonIcon } from '../ButtonIcon';
-import { Container, Info, Title, Quantity, Options } from './styles';
+import firestore from "@react-native-firebase/firestore";
+import React from "react";
+import { ButtonIcon } from "../ButtonIcon";
+import { Container, Info, Options, Quantity, Title } from "./styles";
 
 export type ProductProps = {
   id: string;
   description: string;
   quantity: number;
   done: boolean;
-}
+};
 
 type Props = {
   data: ProductProps;
-}
+};
 
-export function Product({ data }: Props) {
+export const Product: React.FC<Props> = ({ data }) => {
+  async function handleToggleDone() {
+    try {
+      await firestore()
+        .collection<ProductProps>("products")
+        .doc(data.id)
+        .update({ done: !data.done });
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
   return (
     <Container>
       <Info>
-        <Title done={data.done}>
-          {data.description}
-        </Title>
+        <Title done={data.done}>{data.description}</Title>
 
-        <Quantity>
-          Quantidade: {data.quantity}
-        </Quantity>
+        <Quantity>Quantidade: {data.quantity}</Quantity>
       </Info>
 
       <Options>
         <ButtonIcon
           icon={data.done ? "undo" : "check"}
+          onPress={handleToggleDone}
         />
 
-        <ButtonIcon
-          icon="delete"
-          color="alert"
-        />
+        <ButtonIcon icon="delete" color="alert" />
       </Options>
     </Container>
   );
-}
+};
