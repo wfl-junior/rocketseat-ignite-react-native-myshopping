b323da0448b3dd896ea887ae7328920d589c69c8
Wallace Júnior <wflj1997@hotmail.com>
2022-07-22 17:48:08 -0300

    Instalando o pacote de autenticação

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
-import { Container, EmptyPhotoContainer, Image, EmptyPhotoText } from './styles';
+import {
+  Container,
+  EmptyPhotoContainer,
+  EmptyPhotoText,
+  Image,
+} from "./styles";
 
 type Props = TouchableOpacityProps & {
   uri?: string;
-}
+};
 
-export function Photo({ uri, ...rest }: Props) {
-  return (
-    <TouchableOpacity activeOpacity={0.8} {...rest}>
-      <Container>
-        {
-          uri ? <Image source={{ uri }} /> : (
-            <EmptyPhotoContainer>
-              <EmptyPhotoText>
-                clique para adicionar
-                ou para trocar a foto
-              </EmptyPhotoText>
-            </EmptyPhotoContainer >
-          )
-        }
-      </Container>
-    </TouchableOpacity>
-  )
-}
+export const Photo: React.FC<Props> = ({ uri, ...props }) => (
+  <TouchableOpacity activeOpacity={0.8} {...props}>
+    <Container>
+      {uri ? (
+        <Image source={{ uri }} />
+      ) : (
+        <EmptyPhotoContainer>
+          <EmptyPhotoText>Nenhuma imagem selecionada</EmptyPhotoText>
+        </EmptyPhotoContainer>
+      )}
+    </Container>
+  </TouchableOpacity>
+);
